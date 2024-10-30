5cad6b70981f608ead93d59dce882e6099c889a7
Arnav-Bista <arnav.bista12@gmail.com>
2024-10-30 15:08:04 +0000

    iterative!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 
 def main():
-    user_input = input("Hey chat, type 1 if we're computing the fibonacci, 2 for factorials\n> ")
+    user_input = input(
+        "Hey chat, type 1 if we're computing the fibonacci, 2 for factorials\n> ")
     if user_input == "1":
         print(fibonacci(10))
     elif user_input == "2":
@@ -11,7 +12,14 @@ def main():
 
 
 def fibonacci(n: int) -> int:
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    i, j = 0, 1
+    for _ in range(n - 1):
+        i, j = j, i+j
+    return j
 
 
 def factorial(n: int) -> int:
